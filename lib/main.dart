6fc0b569de1d154d706b8e550bb73caccaa66a30
AllanCicceri <allan.cicceri@gmail.com>
2022-05-23 17:28:27 -0300

    First commit: app basic structure

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) => runApp(ExpansesApp());
+
+class ExpansesApp extends StatelessWidget {
+  const ExpansesApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ExpensesApp'),
+      ),
+      body: Center(child: Text('Versão inicial')),
+    );
+  }
+}
