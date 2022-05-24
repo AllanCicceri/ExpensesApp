f119576fb412b2af174527376b1c7f2775627614
AllanCicceri <allan.cicceri@gmail.com>
2022-05-24 15:08:51 -0300

    add: List of expenses with some initial styles

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expanses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main(List<String> args) => runApp(ExpansesApp());
@@ -14,15 +15,66 @@ class ExpansesApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
+
+  final _transactions = [
+    Transaction(
+        id: 't1',
+        title: 'Tela de projeção',
+        value: 110.50,
+        date: DateTime.now()),
+    Transaction(
+        id: 't2',
+        title: 'Camiseta branca botões',
+        value: 90.75,
+        date: DateTime.now()),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('ExpensesApp'),
-      ),
-      body: Center(child: Text('Versão inicial')),
-    );
+        appBar: AppBar(
+          title: Text('ExpensesApp'),
+        ),
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Card(
+                child: Text('Gráfico'),
+              ),
+              Column(
+                children: [
+                  ..._transactions.map((e) => Card(
+                        child: Row(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.symmetric(
+                                  horizontal: 15, vertical: 10),
+                              decoration: BoxDecoration(
+                                  border: Border.all(
+                                      color: Colors.purple, width: 2)),
+                              padding: EdgeInsets.all(10),
+                              child: Text(
+                                e.value.toString(),
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                  color: Colors.purple,
+                                ),
+                              ),
+                            ),
+                            Column(
+                              children: [
+                                Text(e.title),
+                                Text(e.date.toLocal().toString()),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ))
+                ],
+              ),
+            ]));
   }
 }
