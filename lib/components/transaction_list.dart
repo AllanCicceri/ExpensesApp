fa3e96ec5d5467660a37864ea4bd2dfda832c1f1
AllanCicceri <allan.cicceri@gmail.com>
2022-05-27 11:41:58 -0300

    refact: changed SingleChildListView for ListView.builder

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -10,55 +10,54 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 400,
-      padding: EdgeInsets.symmetric(vertical: 10),
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            ...transactions.map((e) => Card(
-                  elevation: 3,
-                  child: Row(
+        height: 400,
+        padding: EdgeInsets.symmetric(vertical: 10),
+        child: ListView.builder(
+          itemCount: transactions.length,
+          itemBuilder: (ctx, index) {
+            final e = transactions[index];
+
+            return Card(
+              elevation: 3,
+              child: Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                            color: Colors.purple.shade200, width: 2)),
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'R\$ ${e.value.toStringAsFixed(2)}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple.shade200,
+                      ),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Container(
-                        margin:
-                            EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                                color: Colors.purple.shade200, width: 2)),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          'R\$ ${e.value.toStringAsFixed(2)}',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.purple.shade200,
-                          ),
+                      Text(
+                        e.title,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
                         ),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            e.title,
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                            ),
-                          ),
-                          Text(
-                            DateFormat('d MMM y').format(e.date),
-                            style: TextStyle(
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ],
+                      Text(
+                        DateFormat('d MMM y').format(e.date),
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
                       ),
                     ],
                   ),
-                ))
-          ],
-        ),
-      ),
-    );
+                ],
+              ),
+            );
+          },
+        ));
   }
 }
